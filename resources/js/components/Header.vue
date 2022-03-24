ec33bc76741ec7686670c89324a24d732125e578
Masadavide <davide.masadm@gmail.com>
2022-03-24 17:10:20 +0100

    init repo di oggi, aggiunto vue

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped>
+body{
+    background-color: blue;
+}
+</style>
